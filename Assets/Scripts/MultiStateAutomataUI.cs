08fbce2997075dccc7154c6abcf04b18157a62ac
DavidCM <dcm.robin@gmail.com>
2024-02-16 17:00:54 +0000

    the viewrule doesn't quite work, but it's almost there

--- Assets/Scripts/MultiStateAutomataUI.cs
+++ Assets/Scripts/MultiStateAutomataUI.cs
@@ -16,6 +16,8 @@ public class MultiStateAutomataUI : MonoBehaviour
     public TMP_Dropdown originalStateDropdown;
     public TMP_Dropdown targetStateDropdown;
 
+    public GameObject viewRuleUI;
+
     public void CreateRule()
     {
         newRule = new CustomRule{
@@ -31,6 +33,7 @@ public class MultiStateAutomataUI : MonoBehaviour
         newRuleGameObject.name = manager.customRules.Count.ToString();
         newRuleGameObject.transform.Find("Text").GetComponent<TMP_Text>().text = "Rule " + manager.customRules.Count;
         newRuleGameObject.transform.Find("deleteButton").GetComponent<Button>().onClick.AddListener(() => DeleteRule(newRuleGameObject.transform.GetSiblingIndex()));
+        newRuleGameObject.GetComponent<Button>().onClick.AddListener(() => ViewRule(1234, 1234, newRule.OriginalState.ToString(), newRule.TargetState.ToString(), Convert.ToInt32(newRuleGameObject.name)));
     }
 
     public void DeleteRule(int n)
@@ -43,6 +46,17 @@ public class MultiStateAutomataUI : MonoBehaviour
                 Destroy(transform.Find("Viewport").Find("Content").GetChild(i).gameObject);
             }
         }
+    }
+
+    public void ViewRule(int nStates, int nAmts, string oState, string tState, int ruleNum)
+    {
+        viewRuleUI.SetActive(true);
+
+        viewRuleUI.transform.Find("NeighborStatesTriggerRuleInputField").GetComponent<TMP_InputField>().text = nStates.ToString();
+        viewRuleUI.transform.Find("NeighborAmtsTriggerRuleInputField").GetComponent<TMP_InputField>().text = nAmts.ToString();
+        viewRuleUI.transform.Find("OriginalState").GetComponent<TMP_InputField>().text = oState;
+        viewRuleUI.transform.Find("TargetState").GetComponent<TMP_InputField>().text = tState;
+        viewRuleUI.transform.Find("Header").GetComponent<TMP_Text>().text = "Rule " + ruleNum.ToString();
     }
 
     private void Start() {
