2a1855bafadbdd7f7c84409c6121471758fb616e
DavidCM <dcm.robin@gmail.com>
2024-02-19 10:45:02 +0000

    you can now generate random amounts of random rules

--- Assets/Scripts/MultiStateAutomataUI.cs
+++ Assets/Scripts/MultiStateAutomataUI.cs
@@ -37,6 +37,50 @@ public class MultiStateAutomataUI : MonoBehaviour
         newRuleGameObject.GetComponent<Button>().onClick.AddListener(() => ViewRule(ArrayToString(manager.customRules[Convert.ToInt32(newRuleGameObject.name)-1].NeighborStatesToTriggerRule), ArrayToString(manager.customRules[Convert.ToInt32(newRuleGameObject.name)-1].NeighborCountsToTriggerRule), manager.customRules[Convert.ToInt32(newRuleGameObject.name)-1].OriginalState.ToString(), manager.customRules[Convert.ToInt32(newRuleGameObject.name)-1].TargetState.ToString(), Convert.ToInt32(newRuleGameObject.name)));
     }
 
+    public void CreateRuleArgs(int[] NCounts, int[] NStates, int OState, int TState)
+    {
+        newRule = new CustomRule{
+            NeighborCountsToTriggerRule = NCounts,
+            NeighborStatesToTriggerRule = NStates,
+            OriginalState = (CellState)OState,
+            TargetState = (CellState)TState
+        };
+
+        manager.customRules.Add(newRule);
+
+        GameObject newRuleGameObject = Instantiate(rulePrefab, transform.Find("Viewport").Find("Content"));
+        newRuleGameObject.name = manager.customRules.Count.ToString();
+        newRuleGameObject.transform.Find("Text").GetComponent<TMP_Text>().text = "Rule " + manager.customRules.Count;
+        newRuleGameObject.transform.Find("deleteButton").GetComponent<Button>().onClick.AddListener(() => DeleteRule(newRuleGameObject.transform.GetSiblingIndex()));
+        newRuleGameObject.GetComponent<Button>().onClick.AddListener(() => ViewRule(ArrayToString(manager.customRules[Convert.ToInt32(newRuleGameObject.name)-1].NeighborStatesToTriggerRule), ArrayToString(manager.customRules[Convert.ToInt32(newRuleGameObject.name)-1].NeighborCountsToTriggerRule), manager.customRules[Convert.ToInt32(newRuleGameObject.name)-1].OriginalState.ToString(), manager.customRules[Convert.ToInt32(newRuleGameObject.name)-1].TargetState.ToString(), Convert.ToInt32(newRuleGameObject.name)));
+    }
+
+    public void RandomRules()
+    {
+        int randomAmt = UnityEngine.Random.Range(2, 16);
+
+        manager.customRules.Clear();
+        for (int i = 0; i < transform.Find("Viewport").Find("Content").childCount; i++)
+        {
+            Destroy(transform.Find("Viewport").Find("Content").GetChild(i).gameObject);
+        }
+
+        for (int i = 0; i < randomAmt; i++)
+        {
+            GenerateRandomRule();
+        }
+    }
+
+    public void GenerateRandomRule()
+    {
+        int[] randomNCounts = RandomIntArray(1, 8);
+        int[] randomNStates = RandomIntArray(1, 4);
+        int randomOState = UnityEngine.Random.Range(0, 4);
+        int randomTstate = UnityEngine.Random.Range(0, 4);
+
+        CreateRuleArgs(randomNCounts, randomNStates, randomOState, randomTstate);
+    }
+
     public void DeleteRule(int n)
     {
         manager.customRules.RemoveAt(n);
@@ -78,6 +122,18 @@ public class MultiStateAutomataUI : MonoBehaviour
         }
 
         return neighborAmtIntArray;
+    }
+
+    public int[] RandomIntArray(int minRange, int maxRange)
+    {
+        int[] randomArray = new int[UnityEngine.Random.Range(minRange, maxRange)];
+
+        for (int i = 0; i < randomArray.Length; i++)
+        {
+            randomArray[i] = UnityEngine.Random.Range(minRange, maxRange);
+        }
+
+        return randomArray;
     }
 
     string ArrayToString(int[] array)
