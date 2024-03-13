5c311c317cc9186b5c10a3e2c81aa39e2d656886
DavidCM <dcm.robin@gmail.com>
2024-03-13 18:12:23 +0000

    made a better loading system

--- Assets/Scripts/Loader.cs
+++ Assets/Scripts/Loader.cs
@@ -7,52 +7,23 @@ using TMPro;
 
 public class Loader : MonoBehaviour
 {
-    public int menuSceneIndex = 0;
-    public int lifeSceneIndex = 1;
-    public int customAutomataSceneIndex = 2;
-    public int multicolorAutomataSceneIndex = 3;
-    public int langtonAntSceneIndex = 4;
-    public int wireworldSceneIndex = 5;
     public TMP_InputField sizeInputfield;
 
     private void Awake() {
         DontDestroyOnLoad(gameObject);
     }
 
-    public void LoadMenu()
+    public void LoadScene(int sceneIndex)
     {
-        Destroy(GameObject.Find("Menu"));
-        Destroy(gameObject);
-        SceneManager.LoadScene(menuSceneIndex);
-    }
-
-    public void LoadLife()
-    {
-        transform.Find("Panel").gameObject.SetActive(false);
-        SceneManager.LoadScene(lifeSceneIndex);
-    }
-
-    public void LoadCreate()
-    {
-        transform.Find("Panel").gameObject.SetActive(false);
-        SceneManager.LoadScene(customAutomataSceneIndex);
-    }
-
-    public void LoadMulticolor()
-    {
-        transform.Find("Panel").gameObject.SetActive(false);
-        SceneManager.LoadScene(multicolorAutomataSceneIndex);
-    }
-
-    public void LoadLangtonAnt()
-    {
-        transform.Find("Panel").gameObject.SetActive(false);
-        SceneManager.LoadScene(langtonAntSceneIndex);
-    }
-
-    public void LoadWireworld()
-    {
-        transform.Find("Panel").gameObject.SetActive(false);
-        SceneManager.LoadScene(wireworldSceneIndex);
+        if (sceneIndex != 0)
+        {
+            transform.Find("Panel").gameObject.SetActive(false);
+        }
+        else
+        {
+            Destroy(GameObject.Find("Menu"));
+            Destroy(gameObject);
+        }
+        SceneManager.LoadScene(sceneIndex);
     }
 }
